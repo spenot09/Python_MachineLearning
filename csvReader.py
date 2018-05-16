3f9064051e6cf515789de16f78f6fa518299b489
spenot09 <34069273+spenot09@users.noreply.github.com>
2018-05-16 17:16:04 +0100

    Delete csvReader.py

--- csvReader.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open("/user/HS127/sc00858/OpenPose/Output/Train/Chew_dataset/datasetChew04.csv") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(" ".join(row))
